8755500e31b256e519a423570f2b59636ce262c2
Eduardo Lonighi <edu_loni3@hotmail.com>
2021-03-19 16:27:25 +0000

    diagnostic on bufferline

--- lua/nv-bufferline/init.lua
+++ lua/nv-bufferline/init.lua
@@ -12,10 +12,10 @@ require'bufferline'.setup{
     max_name_length = 18,
     max_prefix_length = 15, -- prefix used when a buffer is deduplicated
     tab_size = 18,
-    diagnostics = false,   -- false | "nvim_lsp"
-    --diagnostics_indicator = function(count, level)
-    --  return "("..count..")"
-    --end
+    diagnostics = "nvim_lsp",-- false | "nvim_lsp"
+    diagnostics_indicator = function(count, level)
+     return "("..count..")"
+    end,
     show_buffer_close_icons = true,
     show_tab_indicators = true,
     persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
