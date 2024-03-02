8e313d90f43cbc944afb068f419ff9a38851db2e
Eduardo Lonighi <edu_loni3@hotmail.com>
2024-03-02 22:49:26 +0000

    wip

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -16,7 +16,7 @@ return {
         TODO = { icon = ' ', color = 'info' },
         HACK = { icon = ' ', color = 'warning' },
         WARN = { icon = ' ', color = 'warning', alt = { 'WARNING', 'XXX' } },
-        PERF = { icon = ' ', alt = { 'OPTIM', 'PERFORMANCE', 'OPTIMIZE' } },
+        PERF = { icon = '󰊚 ', alt = { 'OPTIM', 'PERFORMANCE', 'OPTIMIZE' } },
         NOTE = { icon = '󱞁 ', color = 'hint', alt = { 'INFO' } },
         TEST = {
           icon = '',
