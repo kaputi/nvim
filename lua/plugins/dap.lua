7b99a72313df5bf273f9c242eb9f083879b5394b
Eduardo Lonighi <edu_loni3@hotmail.com>
2024-04-27 20:11:58 +0100

    wip

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -46,27 +46,5 @@ return {
     -- setup adapters ---------------------------------------------
     -- go is easy because of package
     require('dap-go').setup()
-
-    dap.adapters['pwa-node'] = {
-      type = 'server',
-      host = '127.0.0.1',
-      port = 8123,
-      executable = {
-        command = 'js-debug-adapter',
-      },
-    }
-
-    for _, language in ipairs({ 'javascript', 'typescript' }) do
-      dap.configurations[language] = {
-        {
-          type = 'pwa-node',
-          request = 'launch',
-          name = 'Launch file',
-          program = '${file}',
-          cwd = '${workspaceFolder}',
-          runtimeExecutable = 'node',
-        },
-      }
-    end
   end,
 }
