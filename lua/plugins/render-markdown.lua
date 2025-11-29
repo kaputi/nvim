41400bbc4fb93ca391439a38c271bef624e4d781
Eduardo Lonighi <edu_loni3@hotmail.com>
2025-11-29 12:00:53 +0000

    avante tests

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -6,7 +6,7 @@ return {
   ---@module 'render-markdown'
   ---@type render.md.UserConfig
   opts = {
-    file_types = { 'markdown', 'copilot-chat' },
+    file_types = { 'markdown', 'copilot-chat', 'Avante' },
     preset = 'obsidian',
   },
 }
