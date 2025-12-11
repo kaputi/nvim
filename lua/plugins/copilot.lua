0aaf6e8cd8c2ded34adfbb78d6195896734c5e54
Eduardo Lonighi <edu_loni3@hotmail.com>
2025-12-11 08:56:20 +0000

    copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,19 +1,23 @@
 return {
+  enabled = false, 
   'zbirenbaum/copilot.lua',
   cmd = 'Copilot',
   event = 'InsertEnter',
+  dependencies = {
+    'copilotlsp-nvim/copilot-lsp',
+  },
   config = function()
     require('copilot').setup({
       panel = {
-        enabled = true,
-        auto_refresh = true,
-        -- keymap = {
-        --   jump_prev = '[[',
-        --   jump_next = ']]',
-        --   accept = '<CR>',
-        --   refresh = 'gr',
-        --   open = '<M-CR>',
-        -- },
+        enabled = false,
+        auto_refresh = false,
+        keymap = {
+          --   jump_prev = '[[',
+          --   jump_next = ']]',
+          --   accept = '<CR>',
+          --   refresh = 'gr',
+          -- open = '<M-CR>',
+        },
         -- layout = {
         --   position = 'bottom', -- | top | left | right
         --   ratio = 0.4,
@@ -44,8 +48,31 @@ return {
         cvs = false,
         ['.'] = false,
       },
-      copilot_model = '',
+
+      nes = {
+        enabled = true, -- requires copilot-lsp as a dependency
+        auto_trigger = true,
+        keymap = {
+          accept_and_goto = false,
+          accept = '<leader>c<CR>',
+          dismiss = '<leader>c<BS>',
+          -- accept = 'false',
+          -- dismiss = 'false',
+        },
+      },
     })
+
+    local wk_ok, wk = pcall(require, 'which-key')
+    if wk_ok then
+      wk.register({
+        --   ['<leader>c'] = {
+        --     name = 'Copilot',
+        --     r = { '<cmd>Copilot panel<cr>', 'Open Copilot Panel' },
+        --   },
+        ['<leader>c<CR>'] = { 'Accept Copilot Suggestion', mode = 'n' },
+        ['<leader>c<BS>'] = { 'Dismiss Copilot Suggestion', mode = 'n' },
+      })
+    end
 
     -- local cmp_ok, cmp = pcall(require, 'cmp')
     -- if not cmp_ok then
