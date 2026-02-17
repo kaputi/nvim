de72091c4fdef20f21bf0d166ef998340be6c352
Eduardo Lonighi <edu_loni3@hotmail.com>
2026-02-17 09:26:08 +0000

    close on enter for certain titles

--- lua/plugins/bqf.lua
+++ lua/plugins/bqf.lua
@@ -1,1 +1,37 @@
-return { 'kevinhwang91/nvim-bqf' }
+-- Quickfix titles that should auto-close when selecting an item
+local auto_close_titles = {
+  'References',
+  'references',
+  'Definitions',
+  'Declarations',
+  'Implementations',
+  'Type Definitions',
+  'Incoming Calls',
+  'Outgoing Calls',
+}
+
+return {
+  'kevinhwang91/nvim-bqf',
+  opts = {
+    func_map = {
+      open = '',
+    },
+  },
+  keys = {
+    {
+      '<CR>',
+      function()
+        local title = vim.fn.getqflist({ title = 1 }).title or ''
+        vim.cmd('normal! <CR>')
+        for _, pattern in ipairs(auto_close_titles) do
+          if title:match(pattern) then
+            vim.cmd('cclose')
+            return
+          end
+        end
+      end,
+      ft = 'qf',
+      desc = 'Open item and conditionally close quickfix',
+    },
+  },
+}
