e3aa5ff179743c243d1bbf8d34e8d67b6b150aa7
Eduardo Lonighi <edu_loni3@hotmail.com>
2026-03-25 06:55:18 +0000

    remove auto close temporarily

--- lua/plugins/bqf.lua
+++ lua/plugins/bqf.lua
@@ -14,21 +14,21 @@ return {
   'kevinhwang91/nvim-bqf',
   ft = 'qf',
   opts = {},
-  keys = {
-    {
-      '<CR>',
-      function()
-        local title = vim.fn.getqflist({ title = 1 }).title or ''
-        vim.cmd('.cc')
-        for _, pattern in ipairs(auto_close_titles) do
-          if title:match(pattern) then
-            vim.cmd('cclose')
-            return
-          end
-        end
-      end,
-      ft = 'qf',
-      desc = 'Open item and conditionally close quickfix',
-    },
-  },
+  -- keys = {
+  -- {
+  --   '<CR>',
+  --   function()
+  --     local title = vim.fn.getqflist({ title = 1 }).title or ''
+  --     vim.cmd('.cc')
+  --     for _, pattern in ipairs(auto_close_titles) do
+  --       if title:match(pattern) then
+  --         vim.cmd('cclose')
+  --         return
+  --       end
+  --     end
+  --   end,
+  --   ft = 'qf',
+  --   desc = 'Open item and conditionally close quickfix',
+  -- },
+  -- },
 }
